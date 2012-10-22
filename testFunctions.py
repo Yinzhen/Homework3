00aebc2f65ac512443d1e60b694d621740eac24e
Yinzhen Jin <yinzhenj@princeton.edu>
2012-10-22 16:06:29 -0400

    Add higher order 2 dimens test for Jacobian

--- testFunctions.py
+++ testFunctions.py
@@ -25,25 +25,23 @@ class TestFunctions(unittest.TestCase):
         self.assertEqual(Df_x.shape, (2,2))
         N.testing.assert_array_almost_equal(Df_x, A)
 
-    def testApproxJacobian3(self):
-        for i in range(3, 10):
-            A = N.matrix(N.zeros((i,i)))
-            def f(x):
-                return A*x
-            x0 = N.matrix(N.zeros((i,1)))
-            for j, k in [(j,k) for j in range(i) for k in range(i)]:
-                A[j, k] = (j + k)*0.1
-            x0[:,:] = 0.5
-            dx = 1.e-6
-            Df_x = F.ApproximateJacobian(f, x0, dx)
-            self.assertEqual(Df_x.shape, (i,i))
-            N.testing.assert_array_almost_equal(Df_x, A)
-
     def testPolynomial(self):
         # p(x) = x^2 + 2x + 3
         p = F.Polynomial([1, 2, 3])
         for x in N.linspace(-2,2,11):
             self.assertEqual(p(x), x**2 + 2*x + 3)
+
+    def testAprroxJacobian3(self):
+        def f(x = N.matrix(N.zeros((2,1)))):
+            ans = N.matrix(N.zeros((2,1)))
+            ans[0,0] = x[0,0]**2 + 3.0*x[0,0]*x[1,0] + 4.0*x[1,0]**2
+            ans[1,0] = -2.0*x[0,0]**2 + x[1,0]**2
+            return ans
+        x0 = N.matrix("1.; 2.")
+        dx = 1.e-8
+        Df_x = F.ApproximateJacobian(f, x0, dx)
+        A = N.matrix("8. 19.; -4. 4.")
+        N.testing.assert_array_almost_equal(Df_x, A)
 
 
 if __name__ == '__main__':
