ffc59a4c9320a429662c74c8ecd7db20ea3e3e10
Yinzhen Jin <yinzhenj@princeton.edu>
2012-10-25 23:44:11 -0400

    Add more analytical tests in testNewton

--- testNewton.py
+++ testNewton.py
@@ -41,6 +41,29 @@ class TestNewton(unittest.TestCase):
         f = lambda x: x**2 - x - 6.0
         solver = newton.Newton(f, tol = 1, maxiter = 1, dx = 1.e-6, Df = None)
         x = solver.solve(0.0)
+
+    def testAnalyticSolution1(self):
+        f = lambda x: x**2 - x - 6
+        x0 = N.matrix('1.0')
+        solver = newton.Newton(f, tol = 1.e-9, maxiter = 50, dx = 1.e-3, Df = ["Polynomial", [N.matrix('1.0, -1.0'), N.matrix('2.0, -1')]])
+        x = solver.solve(x0)
+        self.assertEqual(x, 3.0)
+
+    def testAnalyticSolution2(self):
+        def f(x = N.matrix(N.zeros((2,1)))):
+            ans = N.matrix(N.zeros((2,1)))
+            ans[0,0] = 10.0*x[0,0]**2 - x[1,0]**2 - x[0,0] + 1.0
+            ans[1,0] = 2.0*x[0,0] - x[1,0] - 1.0
+            return ans
+
+        df = []
+        df.append([N.matrix("10.0, -1.0, -1.0"), N.matrix("2.0, 0, 1.0; 0.0, 2.0, 0.0")])
+        df.append([N.matrix("2.0, -1.0"), N.matrix("1.0, 0.0; 0.0, 1.0")])
+
+        solver = newton.Newton(f, tol = 1.e-9, maxiter = 40, dx = 1.e-6, Df = ["Polynomial", df[0], df[1]])
+        x0 = N.matrix("2.; -3.")
+        x = solver.solve(x0)
+        N.testing.assert_array_almost_equal(x, N.matrix("0.; -1."))
         
 
 if __name__ == "__main__":
