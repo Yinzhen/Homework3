a9940f576fa765caf92e78e3a1e0fc347fb3e3a7
Yinzhen Jin <yinzhenj@princeton.edu>
2012-10-25 23:33:23 -0400

    Add analytical Jacobian

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import numpy as N
+import math as M
 
 def ApproximateJacobian(f, x, dx=1e-6):
     """Return an approximation of the Jacobian Df(x) as a numpy matrix"""
@@ -12,6 +13,53 @@ def ApproximateJacobian(f, x, dx=1e-6):
         v = N.matrix(N.zeros((n,1)))
         v[i,0] = dx
         Df_x[:,i] = (f(x + v) - fx)/dx
+    return Df_x
+
+def AnalyticJacobian(x, dx=1e-6, function = []):
+    try:
+        n = len(x)
+    except TypeError:
+        n = 1
+    Df_x = N.matrix(N.zeros((n,n)))
+
+    def Df(x, function, i = 0): 
+        if function[0] == "test":
+            print "Having used analytical Jacobian"
+
+        elif function[0] == "Polynomial":
+            coeffs = function[i][0]
+            n = coeffs.shape[1]
+            dimen = len(x)
+            ans = N.matrix(N.zeros((1, dimen)))
+            order_coeff = N.matrix(N.zeros((n, 1)))
+            for j in range(dimen):
+
+                deriv = function[i][1].copy()
+                order = function[i][1].copy()
+                order[j, :] = order[j, :] - 1.0
+                for k1 in range(n): 
+                    temp = 1 
+                    for k2 in range(dimen):
+                        temp = temp*M.pow(x[k2], order[k2,k1])
+                    ans[0, j] = ans[0, j] + coeffs[0, k1]*deriv[j, k1]*temp
+            return ans
+
+        elif function[0] == "SimpleComplex":
+            dimen = len(x)
+            ans = N.matrix(N.zeros((1, dimen)))
+            for j in range(dimen):
+                coeffs = function[i][j]
+                for para in coeffs.keys():
+                    df = BasicDerivative(para)
+                    ans[0, j] = ans[0, j] + coeffs[para]*df(x[i-1])
+            return ans
+
+        else:
+            print "There is no analytical Jacobian, please chage Df in test function as False"
+
+
+    for i in range(n):
+        Df_x[i,:] = Df(x, function, i+1)
     return Df_x
 
 class Polynomial(object):
@@ -38,3 +86,33 @@ class Polynomial(object):
     def __call__(self, x):
         return self.f(x)
 
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+class BasicDerivative(object):
+
+    def __init__(self, function):
+        self._function = function
+
+    def __repr__(self):
+        return "BasicDerivative(%s)" % (self._function)
+
+    def f(self, x):
+        if self._function == "sin":
+            return M.cos(x)
+        elif self._function == "cos":
+            return -M.sin(x)
+        elif self._function == "tan":
+            return M.pow(1.0/M.tan(x), -2)
+        elif is_number(self._function):
+            return M.pow(x, float(self._function) - 1)
+        else:
+            raise Exception("No such derivative function")
+
+    def __call__(self, x):
+        return self.f(x)
